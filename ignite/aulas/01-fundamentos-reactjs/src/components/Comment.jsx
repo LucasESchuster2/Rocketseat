03c82500fccf3b6b12d7fe4294d83c5a30bcd4c4
Lucas Eduardo Schuster <lucas.schuster@sementesroos.com.br>
2024-05-15 10:24:43 -0300

    Realizando likes nos comentários

--- ignite/aulas/01-fundamentos-reactjs/src/components/Comment.jsx
+++ ignite/aulas/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,9 +1,15 @@
 import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
 import { Avatar } from "./Avatar";
+import React, { useState } from "react";
 export function Comment({content, onDeleteComment}) {
   function handleDeleteComment(){
     onDeleteComment(content)
+  }
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleLikeComment(){
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -27,9 +33,9 @@ export function Comment({content, onDeleteComment}) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp/>
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
