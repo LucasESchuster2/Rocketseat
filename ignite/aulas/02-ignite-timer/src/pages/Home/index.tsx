ca9a7671a3dbc8b6874201352de33da082eac5e3
Lucas Eduardo Schuster <lucas.schuster@sementesroos.com.br>
2024-05-16 11:24:13 -0300

    Página: Home

---
+++ ignite/aulas/02-ignite-timer/src/pages/Home/index.tsx
@@ -0,0 +1,34 @@
+import { Play } from 'phosphor-react'
+import {
+  ContdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+          <label htmlFor="minutesAmount">Durante</label>
+          <input id="minutesAmount" type="number" />
+          <span>minutos.</span>
+        </FormContainer>
+        <ContdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </ContdownContainer>
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
+}
