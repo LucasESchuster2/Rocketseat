f126c801959bd8ddc3b58571aefb8f2c26fd1476
Lucas Eduardo Schuster <lucas.schuster@sementesroos.com.br>
2024-05-16 13:49:28 -0300

    Componente: Status

--- ignite/aulas/02-ignite-timer/src/pages/Home/index.tsx
+++ ignite/aulas/02-ignite-timer/src/pages/Home/index.tsx
@@ -9,18 +9,32 @@ import {
   TaskInput,
 } from './styles'
 
+
 export function Home() {
   return (
     <HomeContainer>
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput placeholder="Dê um nome para o seu projeto" id="task" />
+          <TaskInput
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+            id="task"
+          />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"></option>
+            <option value="Proeto 2"></option>
+            <option value="Projeto 4"></option>
+            <option value="Banana"></option>
+          </datalist>
           <label htmlFor="minutesAmount">Durante</label>
           <MinutesAmountInput
             placeholder="00"
             id="minutesAmount"
             type="number"
+            step={5}
+            min={5}
+            max={60}
           />
           <span>minutos.</span>
         </FormContainer>
