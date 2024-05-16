778575e3d36ba5b11bfa5de54bed5b827fc4670a
Lucas Eduardo Schuster <lucas.schuster@sementesroos.com.br>
2024-05-16 08:32:24 -0300

    Cores & fonte

--- ignite/aulas/02-ignite-timer/src/App.tsx
+++ ignite/aulas/02-ignite-timer/src/App.tsx
@@ -2,17 +2,18 @@ import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
 import { defaultTheme } from "./styles/themes/default";
 import styles from "./App.module.css";
+import { GlobalStyle } from "./styles/global";
 export function App() {
   return (
     <>
       <ThemeProvider theme={defaultTheme}>
-        <div>
+    
           <Button variant="primary" />
           <Button variant="secondary" />
           <Button variant="success" />
           <Button variant="danger" />
           <Button />
-        </div>
+      <GlobalStyle/>
       </ThemeProvider>
     </>
   );
