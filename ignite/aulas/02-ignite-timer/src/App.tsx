6ca829128dd2d1b324b71192ae25805010ba9c26
LukasSchusta <lukinhasschusta@gmail.com>
2024-05-15 22:05:44 -0300

    Tipagem de temas

--- ignite/aulas/02-ignite-timer/src/App.tsx
+++ ignite/aulas/02-ignite-timer/src/App.tsx
@@ -1,13 +1,19 @@
+import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
-
+import { defaultTheme } from "./styles/themes/default";
+import styles from "./App.module.css";
 export function App() {
   return (
     <>
-      <Button variant="primary"/>
-      <Button variant="secondary"/>
-      <Button variant="success"/>
-      <Button variant="danger"/>
-      <Button/>
+      <ThemeProvider theme={defaultTheme}>
+        <div>
+          <Button variant="primary" />
+          <Button variant="secondary" />
+          <Button variant="success" />
+          <Button variant="danger" />
+          <Button />
+        </div>
+      </ThemeProvider>
     </>
-  )
+  );
 }
--- ignite/aulas/02-ignite-timer/src/components/Button.styles.ts
+++ ignite/aulas/02-ignite-timer/src/components/Button.styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
 export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
 
@@ -16,7 +16,14 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
-  ${props => {
-    return `background-color: ${buttonVariants[props.variant]}`
-  }}
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
+
+  background-color: ${props => props.theme.primary};
+  color: ${props => props.theme.white}
+
+  /* ${props => {
+    return css`background-color: ${buttonVariants[props.variant]}`
+  }} */
 `
