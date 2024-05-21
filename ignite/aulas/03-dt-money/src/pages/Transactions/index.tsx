f67919da0de3a2895538582665db9253fc1fa139
Lucas Eduardo Schuster <lucas.schuster@sementesroos.com.br>
2024-05-21 16:41:56 -0300

    Criando hook para o resumo

--- ignite/aulas/03-dt-money/src/pages/Transactions/index.tsx
+++ ignite/aulas/03-dt-money/src/pages/Transactions/index.tsx
@@ -8,6 +8,7 @@ import {
   TransactionsTable,
 } from "./styles";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 
 export function Transactions() {
@@ -27,11 +28,12 @@ export function Transactions() {
                   <td width="50%">{transaction.description}</td>
                   <td>
                     <PriceHighLight variant={transaction.type}>
-                      {transaction.price}
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighLight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.created_at}</td>
+                  <td>{dateFormatter.format(new Date(transaction.created_at))}</td>
                 </tr>
               );
             })}
