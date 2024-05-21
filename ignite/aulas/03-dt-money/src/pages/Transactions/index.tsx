d3240231ed58261e129fc9638c4cf2a5c36e3e84
Lucas Eduardo Schuster <lucas.schuster@sementesroos.com.br>
2024-05-21 15:59:29 -0300

    Calculando resumo de transações

--- ignite/aulas/03-dt-money/src/pages/Transactions/index.tsx
+++ ignite/aulas/03-dt-money/src/pages/Transactions/index.tsx
@@ -1,29 +1,40 @@
+import { useContext, useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
-import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+
 
 export function Transactions() {
+  const { transactions } = useContext(TransactionsContext)
+
   return (
     <div>
       <Header />
       <Summary />
       <TransactionsContainer>
-        <SearchForm/>
+        <SearchForm />
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-            <td>  <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight></td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-            <tr>
-              <td width="50%">Hamburger</td>
-             <td> <PriceHighLight variant="outcome">- R$ 59,00</PriceHighLight></td>
-              <td>Venda</td>
-              <td>10/04/2022</td>
-            </tr>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighLight variant={transaction.type}>
+                      {transaction.price}
+                    </PriceHighLight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.created_at}</td>
+                </tr>
+              );
+            })}
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
